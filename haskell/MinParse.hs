c9806cad90033c3858acfca48e0cedd010963178
Scott Olsen <scott.c.olsen@oracle.com>
2017-11-03 14:39:29 -0400

    Abstracted out Flag data type and other functions

--- haskell/MinParse.hs
+++ haskell/MinParse.hs
@@ -1,12 +1,28 @@
 module MinParse (
+                  Flag(..),
                   parsed,
                   parsedOpts,
                   nonOpts,
-                  parse  
+                  parse,
+                  flgVal,
+                  optVal,
+                  putversion,
+                  puthelp,
+                  displayhelp
                 ) where
 
 import System.Console.GetOpt
 import Data.Maybe
+import System.IO
+import System.Exit
+import Data.List
+
+data Flag = Verbose 
+          | Help String
+          | Version String
+          | Flg
+          | Opt String 
+           deriving (Eq, Show)
 
 parsed :: ([a], [String], [String]) -> ([a], [String])
 parsed trip = (parsedOpts trip, nonOpts trip)
@@ -20,4 +36,28 @@ nonOpts (_, _, errs) = errs
 
 parse :: [OptDescr a] -> [String] -> ([a], [String], [String])
 parse options args = getOpt RequireOrder options args
+
+optVal :: Flag -> Maybe String
+optVal (Opt a) = Just a
+optVal _ = Nothing
+
+flgVal :: Flag -> Maybe Bool
+flgVal (Flg) = Just True
+flgVal _ = Just False
+
+puthelp :: Maybe Flag -> IO ()
+puthelp (Just (Help a))= putStrLn a
+                   >> exitWith(ExitFailure 1)
+puthelp Nothing = putStrLn "No help documentation provided."
+
+putversion :: Maybe Flag -> IO ()
+putversion (Just (Version a)) = putStrLn a
+                         >> exitWith(ExitFailure 1)
+putversion Nothing = putStrLn "Unknown Version"
+
+-- Courtesy Functions
+--Take a parsed and find and display help.
+displayhelp :: String -> ([Flag], [String]) -> IO ()
+displayhelp x (a, _) = puthelp (find (== Help x) a)
+diaplyhelp _ (_, _) = putStrLn "Error. No help text provided"
 
