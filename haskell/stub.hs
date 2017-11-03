3dbae36389e094ed28650fd4be15cf334db8fb86
Scott Olsen <scg.olsen@gmail.com>
2017-11-02 23:52:37 -0400

    Update stub.hs

--- haskell/stub.hs
+++ haskell/stub.hs
@@ -4,12 +4,40 @@
 import System.IO
 import Control.Monad
 import System.Environment 
+import System.Exit
+import System.Console.GetOpt
+import Data.Maybe
 
-reval = map reverse . lines
+-- Option Helpers
+-- NOTE: OptArgs must use java like argument syntax on the command line:
+-- e.g. output=blah. Otherwise, the set default will be used.
 
-parseArg :: String -> a 
-parseArg arg
-    | arg == "-h" = putStrLn "hi" 
-    
+parsed :: ([a], [String], [String]) -> ([a], [String])
+parsed trip = (parsedOpts trip, nonOpts trip)
+
+parsedOpts :: ([a], [String], [String]) -> [a]
+parsedOpts (a, _, _) = a
+
+nonOpts :: ([a], [String], [String]) -> [String]
+nonOpts (_, a, []) = a
+nonOpts (_, _, errs) = errs
+
+data Flag = Verbose 
+          | Version
+          | Help
+          | Output String
+          deriving (Eq, Show)
+
+ducks :: [OptDescr Flag]
+ducks =   [ Option ['v'] ["verbose"] (NoArg Verbose) "Chatty output",
+            Option ['h'] ["help"] (NoArg Help) "Help",
+            Option ['o'] ["output"] (OptArg (Output . fromMaybe "stdout") "OUT") "Output"
+          ]
+
+parse :: [OptDescr Flag] -> [String] -> ([Flag], [String], [String])
+parse options args = getOpt RequireOrder options args
+
 main = getArgs
-       >>= mapM_ parseArgs . words
+       >>= return . parsed . parse ducks
+       >>= print
+
