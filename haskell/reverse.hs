b18fd2e31ffe6596908131283e302f8a50400692
Scott Olsen <scott.c.olsen@oracle.com>
2017-11-07 17:19:57 -0500

    Update reverse to use record syntax

--- haskell/reverse.hs
+++ haskell/reverse.hs
@@ -17,19 +17,18 @@ version = "1.0.0"
 help = "Usage: ..."
 
 options :: [OptDescr Flag]
-options =  [ noArg ['v'] ["verbose"] True "Chatty output",
+options =  [ 
              Option ['V'] ["version"] (NoArg (Version version)) "Version",
              Option ['h'] ["help"] (NoArg (Help help)) "Help",
-             optArg ['o'] ["output"] "stdout" "OUT" "Output",
-             optArg ['d'] ["dragon"] "other" "OUT" "Output",
-             optArg ['t'] ["test"] "test" "TET" "this is a test"
+             option (params {charIdens = ['a'], stringIdens = ["all"], useText = "allero", defaultArg = "hi", argType = "blah"}),
+             option (params {charIdens = ['q'], stringIdens = ["question"], useText = "Ask a question"})
            ]
 
 rvrs = map reverse 
 
 main = parseArgs options
        >>= \parsed -> print (getAllIdentifiers parsed)
-       >>= \_      -> print (getArgByIden parsed "dragon")
+       >>= \_      -> print (getArgByIden parsed "all")
        >>= \_      -> return (rvrs (parsedNonOpts parsed))
        >>= print
        
